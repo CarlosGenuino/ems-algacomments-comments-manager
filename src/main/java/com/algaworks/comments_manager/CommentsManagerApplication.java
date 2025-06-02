4c4da7fac2800e62b5f118386f2281e5b0728421
Carlos Genuino <carloxsgenuino3@live.com>
2025-06-02 09:13:16 -0300

    Adding new project files

---
+++ src/main/java/com/algaworks/comments_manager/CommentsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.comments_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsManagerApplication.class, args);
+	}
+
+}
